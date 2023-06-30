96253dac24864fd706f41c727f1a28aaf5bda657
Gregory <gregory@stopdesign.ru>
2023-06-29 20:14:02 -0700

    endless process

--- src/main/management/commands/process.py
+++ src/main/management/commands/process.py
@@ -1,24 +1,18 @@
 import logging
 from datetime import datetime, timedelta, timezone
+from time import sleep
 
 from django.core.management.base import BaseCommand
 
 from main.ibkr_api_hook import process_webhook_call
-from main.telegram_hook import tg_alert
 from main.models import WebhookCall
+from main.telegram_hook import tg_alert
 
 log = logging.getLogger("process")
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument("-rt", dest="reset_token", action="store_true")
-        parser.add_argument("-a", "--all", dest="process_all", action="store_true")
-
-    def handle(self, *args, **kwargs):
-        process_all = kwargs["process_all"]
-        reset_token = kwargs["reset_token"] or False
-
+    def process(self):
         now = datetime.utcnow().replace(tzinfo=timezone.utc)
         too_late = now - timedelta(minutes=3)
         hooks = WebhookCall.objects.filter(
@@ -26,16 +20,23 @@ class Command(BaseCommand):
             success__isnull=True,
         ).order_by("timestamp")
 
-        log.info(f"Hooks to process: {len(hooks)}")
-
-        if not process_all:
-            hooks = hooks[:1]
-
         for hook in hooks:
             log.info(f"Process: {hook}, {hook.webhook.mode}")
             if hook.webhook.mode in ["PREVIEW", "ORDER"]:
-                process_webhook_call(hook, reset_token=reset_token)
+                process_webhook_call(hook)
             else:
                 tg_alert(hook)
+
+    def handle(self, *args, **kwargs):
+        while True:
+            try:
+                self.process()
+                sleep(10)
+            except KeyboardInterrupt:
+                print()
+                break
+            except Exception as e:
+                log.exception(e)
+                sleep(30)
 
         log.info(f"DONE")
